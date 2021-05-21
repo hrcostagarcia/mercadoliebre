d17d66054fea131f475995a551c62b17065e4c2a
hrcostagarcia <hrcostagarcia@gmail.com>
2021-05-21 13:47:02 -0300

    v4 with procfile and app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ console.log(publicPath) // nos da por consola la ruta del public
 
 app.use(express.static(publicPath));
 
-app.listen(3060, () => console.log('Servidor corriendo ')) // levantamos una server en localhost
+app.listen(process.env.PORT || 3060, () => console.log('Servidor corriendo ')) // levantamos una server en localhost
 
 app.get('/', (req, res) => {
 
